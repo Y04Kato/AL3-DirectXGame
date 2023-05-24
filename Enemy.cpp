06dda32dd020194a1c51b07130d1de495382925c
k022g0046@g.neec.ac.jp <k022g0046@g.neec.ac.jp>
2023-05-24 14:54:19 +0900

    test

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,6 +1,6 @@
 #include "Enemy.h"
-#include <cassert>
 #include "Affin.h"
+#include <cassert>
 
 void Enemy::Initialize(Model* model, const Vector3& position) {
 	assert(model);
@@ -12,27 +12,22 @@ void Enemy::Initialize(Model* model, const Vector3& position) {
 	worldTransform_.Initialize();
 
 	worldTransform_.translation_ = position;
+
+	move_ = {0, 0, 0};
+	kCharacterSpeed_ = 0.001f;
+
+	static_cast<size_t>(phase_);
 }
 
 void Enemy::Update() {
-	Vector3 move = {0, 0, 0};
-	const float kCharacterSpeed = 0.2f;
-	switch (phase_) { 
-	case Phase::Approach:
-	default:
-		//移動
-		move.z -= kCharacterSpeed;
-		if (worldTransform_.translation_.z < 0.0f) {
-			phase_ = Phase::Leave;
-		}
-		break;
-	case Phase::Leave:
-		move.x -= kCharacterSpeed;
-		move.y += kCharacterSpeed;
-		break;
+
+	(this->*phasetable_[0])();
+
+	if (worldTransform_.translation_.z < 0.0f) {
+		(this->*phasetable_[1])();
 	}
 
-	worldTransform_.translation_ = VectorMultiply(worldTransform_.translation_, move);
+	worldTransform_.translation_ = VectorMultiply(worldTransform_.translation_, move_);
 
 	// ワールドトランスフォームの更新
 	worldTransform_.UpdateMatrix();
@@ -41,4 +36,19 @@ void Enemy::Update() {
 void Enemy::Draw(const ViewProjection& viewProjection) {
 	// モデルの描画
 	model_->Draw(worldTransform_, viewProjection, textureHandle_);
+}
+
+void (Enemy::*Enemy::phasetable_[])() = {
+    &Enemy::Approach, // 接近
+    &Enemy::Leave     // 離脱
+};
+
+void Enemy::Approach() {
+	// 移動
+	move_.z -= kCharacterSpeed_;
+}
+
+void Enemy::Leave() {
+	move_.x -= kCharacterSpeed_;
+	move_.y += kCharacterSpeed_;
 }
